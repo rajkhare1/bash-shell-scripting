1364fd9a7302c2b4ac7af71064c1fc5d606e5c3b
rajkhare1 <raj.khare.java@gmail.com>
2024-01-30 07:38:23 +0530

    30-01-2024:update

---
+++ shell-scripts/conditions.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "hello..."
+
+if [ 3 -eq 5 ]; then
+   echo "yes they are the same"
+fi
+
+echo "continue with your program"
+echo "bye"
---
+++ shell-scripts/elseifcond.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+read -p "type a integer between 1 and 4: " num
+
+if [ $num ==  1 ]; then
+   echo "typed 1"
+  elif [ $num == 2 ]; then
+     echo "typed 2"
+   elif [ $num == 3 ]; then
+     echo "typed 3"
+   elif [ $num == 4 ]; then
+     echo "typed 4"
+   else
+     echo "none of the above"
+
+fi
+
+echo "bye"
